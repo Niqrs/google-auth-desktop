b6875f25d82f8c96302f4dadca52e887d28faede
Niqrs <niqr.dev@gmail.com>
2023-03-20 19:10:27 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("multiplatform") version "1.8.0"
+}
+
+group = "me.niqr"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(8)
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    sourceSets {
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
