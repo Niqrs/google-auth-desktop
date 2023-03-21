b1140cc7edc88f124e4f61132d58c8be10427faf
Niqrs <niqr.dev@gmail.com>
2023-03-21 21:21:23 +0300

    Add Ktor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+val ktor_version: String by project
+val logback_version: String by project
+
 plugins {
     kotlin("multiplatform") version "1.8.0"
+    kotlin("plugin.serialization") version "1.8.0"
     id("convention.publication")
 }
 
@@ -19,7 +23,18 @@ kotlin {
         }
     }
     sourceSets {
-        val jvmMain by getting
+        val jvmMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-core:$ktor_version")
+                implementation("io.ktor:ktor-client-cio:$ktor_version")
+                implementation("io.ktor:ktor-client-auth:$ktor_version")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+                implementation("io.ktor:ktor-server-core:$ktor_version")
+                implementation("io.ktor:ktor-server-netty:$ktor_version")
+                implementation("ch.qos.logback:logback-classic:$logback_version")
+            }
+        }
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test"))
