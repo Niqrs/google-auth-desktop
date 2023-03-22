e43d59f41f82b42b40b5a15b2c77d7b70876bc2b
Niqrs <niqr.dev@gmail.com>
2023-03-22 06:30:38 +0300

    Create GoogleAuthClient

---
+++ src/jvmMain/kotlin/api/GoogleAuthClient.kt
@@ -0,0 +1,36 @@
+package api
+
+import GoogleAuthServer
+import googleAuthHttpClient
+import io.ktor.client.call.*
+import io.ktor.client.plugins.auth.providers.*
+import io.ktor.client.request.forms.*
+import models.TokenInfo
+import utils.GoogleOAuthAuthUrl
+import utils.GoogleOAuthTokenUrl
+import utils.buildAuthorizationUrlQuery
+import utils.buildTokenValidationUrl
+
+class GoogleAuthClient(
+    private val googleClientId: String,
+    private val googleClientSecret: String,
+    private val port: Int = 7878
+) {
+    private val authorizationUrlQuery = buildAuthorizationUrlQuery(googleClientId, port)
+    private val bearerTokenStorage = mutableListOf<BearerTokens>()
+    private val client = googleAuthHttpClient(bearerTokenStorage, googleClientId)
+
+    init {
+        println("$GoogleOAuthAuthUrl?$authorizationUrlQuery")
+    }
+
+    suspend fun waitForOAuthIdToken(): String {
+        val server = GoogleAuthServer(port)
+        val code = server.startServerForCode()
+        val tokenInfo: TokenInfo = client.submitForm(
+            url = GoogleOAuthTokenUrl,
+            formParameters = buildTokenValidationUrl(googleClientSecret, googleClientId, code, port)
+        ).body()
+        return tokenInfo.idToken
+    }
+}
