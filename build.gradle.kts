692867522fbf89b058c8b60a355380dc0f8409da
Niqrs <niqr.dev@gmail.com>
2023-03-22 06:26:47 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 val ktor_version: String by project
 val logback_version: String by project
+val slf4j_version: String by project
 
 plugins {
     kotlin("multiplatform") version "1.8.0"
@@ -32,7 +33,7 @@ kotlin {
                 implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
                 implementation("io.ktor:ktor-server-core:$ktor_version")
                 implementation("io.ktor:ktor-server-netty:$ktor_version")
-                implementation("ch.qos.logback:logback-classic:$logback_version")
+                implementation("org.slf4j:slf4j-nop:$slf4j_version")
             }
         }
         val jvmTest by getting {
