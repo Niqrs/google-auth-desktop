dccbed27de407e73a5faa3e2052638628bbad543
Niqrs <niqr.dev@gmail.com>
2023-03-22 06:29:21 +0300

    Create GoogleAuthServer

---
+++ src/jvmMain/kotlin/GoogleAuthServer.kt
@@ -0,0 +1,28 @@
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.delay
+
+internal class GoogleAuthServer(port: Int) {
+    private var code: String? = null
+    private val server = embeddedServer(Netty, port) {
+        routing {
+            get("/") {
+                code = call.request.queryParameters["code"]
+                call.respondText("Authorization code: $code")
+            }
+        }
+    }
+
+    suspend fun startServerForCode(): String {
+        code = null
+        server.start()
+        while (code == null) {
+            delay(500)
+        }
+        server.stop()
+        return code!!
+    }
+}
